9b8d7286af28149ff05a6842cd710d1ed4b30bda
Stephen Farry <stephen.farry@cern.ch>
2018-07-22 08:34:21 +0100

    added admin style and some forms

---
+++ themovieroom/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from films.models import *
+
+class UpdateHome(forms.Form):
+    film    = forms.ModelChoiceField(queryset=review.objects.order_by('-created_date'), initial = review.objects.order_by('-created_date')[0])
+    #feature = forms.ModelChoiceField(queryset=feature.objects.all())
+
