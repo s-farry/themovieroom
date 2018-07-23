61fe42e06747a2dca71c0e01f27cbbea2e809cda
Stephen Farry <stephen.farry@cern.ch>
2018-07-23 11:13:47 +0100

    some local changes

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -7,3 +7,27 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 wsgi = imp.load_source('wsgi', 'themovieroom/wsgi.py')
 application = wsgi.application
+
+# Set this to your root
+SCRIPT_NAME = 'admin'
+
+class PassengerPathInfoFix(object):
+    """
+    Sets PATH_INFO from REQUEST_URI since Passenger doesn't provide it.
+    """
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        from urllib.parse import unquote
+        environ['SCRIPT_NAME'] = SCRIPT_NAME
+
+        request_uri = unquote(environ['REQUEST_URI'])
+        script_name = unquote(environ.get('SCRIPT_NAME', ''))
+        offset = request_uri.startswith(script_name) and len(environ['SCRIPT_NAME']) or 0
+        environ['PATH_INFO'] = request_uri[offset:].split('?', 1)[0]
+        return self.app(environ, start_response)
+
+
+application = get_wsgi_application()
+application = PassengerPathInfoFix(application)
