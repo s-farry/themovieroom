eb27f009ddb7a81ca5445591e6f54570e0ac1ee9
Stephen Farry <stephen.farry@themovieroom.co.uk>
2019-08-26 21:54:52 +0200

    removed some errors

--- themovieroom/forms.py
+++ themovieroom/forms.py
@@ -4,7 +4,7 @@ from films.models import *
 class UpdateHome(forms.Form):
     query = review.objects.filter(status='p')
     film    = forms.ModelChoiceField(queryset=review.objects.order_by('-created_date'))
-    if len(query) > 0:
-        film.initial = query.order_by('-created_date')[0]
+    #if len(query) > 0:
+    #    film.initial = query.order_by('-created_date')[0]
     #feature = forms.ModelChoiceField(queryset=feature.objects.all())
 
