792f4bd5cdf7d3a5071e6092b5d477140e76f916
Stephen Farry <stephen.farry@themovieroom.co.uk>
2019-10-05 18:38:58 +0200

    added www to trusted hosts

--- themovieroom/settings.py
+++ themovieroom/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 if 'DJANGO_DEBUG' in os.environ and os.environ['DJANGO_DEBUG'] == "1":
     DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', '192.168.1.100','themovieroom.co.uk']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', '192.168.1.100','themovieroom.co.uk','www.themovieroom.co.uk']
 
 # Application definition
 
